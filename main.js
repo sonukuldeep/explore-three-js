1586ebc875ad35fadf204ba32fb11025df20462f
sonukuldeep <sonukuldeep@outlook.com>
2023-06-03 21:59:34 +0530

    update on cube

--- main.js
+++ main.js
@@ -1,35 +1,44 @@
 import * as THREE from 'three'
 import WebGL from 'three/addons/capabilities/WebGL.js';
 
+// Scene
 const scene = new THREE.Scene();
+scene.background = new THREE.Color('#242424')
+
+// Camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
 camera.position.z = 5;
 
+// Renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+document.body.appendChild(renderer.domElement);
+
+// Creating a cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ wireframe: true });
+const cube = new THREE.Mesh(geometry, material);
+
+// add cube to scene
+scene.add(cube);
+
+// animate and render
 function animate() {
-  requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
 }
 
-if ( WebGL.isWebGLAvailable() ) {
+if (WebGL.isWebGLAvailable()) {
 
-	// Initiate function or other initializations here
-	animate();
+    // Initiate function or other initializations here
+    animate();
 
 } else {
 
-	const warning = WebGL.getWebGLErrorMessage();
-  document.body.appendChild(warning);
+    const warning = WebGL.getWebGLErrorMessage();
+    document.body.appendChild(warning);
 
 }
