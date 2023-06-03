4b61c60a88ef13e80a0e5501cce85149c31e93c3
sonukuldeep <sonukuldeep@outlook.com>
2023-06-03 17:43:29 +0530

    line

--- main.js
+++ main.js
@@ -1,35 +1,23 @@
 import * as THREE from 'three'
-import WebGL from 'three/addons/capabilities/WebGL.js';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500)
+camera.position.set(0, 0, 100)
+camera.lookAt(0, 0, 0)
+const scene = new THREE.Scene()
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const material = new THREE.LineBasicMaterial({ color: 0x0000ff })
 
-camera.position.z = 5;
+const points = []
+points.push(new THREE.Vector3(-10, 0, 0))
+points.push(new THREE.Vector3(0, 10, 0))
+points.push(new THREE.Vector3(10, 0, 0))
 
-function animate() {
-  requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  renderer.render(scene, camera);
-}
+const geometry = new THREE.BufferGeometry().setFromPoints(points)
 
-if ( WebGL.isWebGLAvailable() ) {
-
-	// Initiate function or other initializations here
-	animate();
-
-} else {
-
-	const warning = WebGL.getWebGLErrorMessage();
-  document.body.appendChild(warning);
-
-}
+const line = new THREE.Line(geometry, material)
+scene.add(line)
+renderer.render(scene, camera)
