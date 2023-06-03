d560d798deea1eb1844707c3619854d82bfc7cc9
sonukuldeep <sonukuldeep@outlook.com>
2023-06-03 18:11:16 +0530

    line

--- main.js
+++ main.js
@@ -21,3 +21,12 @@ const geometry = new THREE.BufferGeometry().setFromPoints(points)
 const line = new THREE.Line(geometry, material)
 scene.add(line)
 renderer.render(scene, camera)
+
+// function animate() {
+//   requestAnimationFrame(animate)
+//   line.rotation.x += 0.01
+//   line.rotation.y += 0.01
+//   renderer.render(scene, camera)
+// }
+
+// animate()
