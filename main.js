8ed885f48276c901a9af0b787cc79930d52cadce
sonukuldeep <sonukuldeep@outlook.com>
2023-06-04 10:54:09 +0530

    debug

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
 import WebGL from 'three/addons/capabilities/WebGL.js';
+import { GUI } from 'dat.gui';
 
 // Scene
 const scene = new THREE.Scene();
@@ -8,6 +9,10 @@ scene.background = new THREE.Color('#242424')
 // Camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
+
+// Adding UI to debug and experimenting different values
+// UI
+const gui = new GUI()
 
 // Renderer
 const renderer = new THREE.WebGLRenderer();
@@ -21,10 +26,18 @@ const material = new THREE.MeshBasicMaterial({
     color: 0xd23d32,
     wireframe: true,
 });
+
+// adding material to GUI
+gui.add(material, 'wireframe')
 const cube = new THREE.Mesh(geometry, material);
 
 // add cube to scene
 scene.add(cube);
+
+// adding cube position to GUI
+gui.add(cube.position, 'x').name('axis-x').min(0).max(10).step(1)
+gui.add(cube.position, 'y').name('axis-y').min(0).max(10).step(1)
+gui.add(cube.position, 'z').name('axis-z').min(0).max(10).step(1)
 
 // animate and render
 function animate() {
