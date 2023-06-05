3c58e64385e848563e30e2d1da4adb878f67234b
sonukuldeep <sonukuldeep@outlook.com>
2023-06-05 18:52:02 +0530

    trackball controls

--- main.js
+++ main.js
@@ -1,47 +1,98 @@
 import * as THREE from 'three'
-import WebGL from 'three/addons/capabilities/WebGL.js';
+import { GUI } from 'dat.gui';
+import { TrackballControls } from 'three/addons/controls/TrackballControls.js';
+import Stats from 'stats.js';
 
-// Scene
-const scene = new THREE.Scene();
-scene.background = new THREE.Color('#242424')
-
-// Camera
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
-
-// Renderer
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-document.body.appendChild(renderer.domElement);
-
-// Creating a cube
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({
-    color: 0xd23d32,
-    wireframe: true,
-});
-const cube = new THREE.Mesh(geometry, material);
-
-// add cube to scene
-scene.add(cube);
-
-// animate and render
-function animate() {
-    requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    renderer.render(scene, camera);
+let perspectiveCamera, orthographicCamera, controls, scene, renderer, stats
+const params = {
+    orthographicCamera: false
 }
-
-if (WebGL.isWebGLAvailable()) {
-
-    // Initiate function or other initializations here
-    animate();
-
-} else {
-
-    const warning = WebGL.getWebGLErrorMessage();
-    document.body.appendChild(warning);
-
+const frustumSize = 400
+init()
+animate()
+function init() {
+    const aspect = window.innerWidth / window.innerHeight
+    perspectiveCamera = new THREE.PerspectiveCamera(60, aspect, 1, 1000)
+    perspectiveCamera.position.z = 500
+    orthographicCamera = new THREE.OrthographicCamera(
+        (frustumSize * aspect) / -2,
+        (frustumSize * aspect) / 2,
+        frustumSize / 2,
+        frustumSize / -2,
+        1,
+        1000
+    )
+    orthographicCamera.position.z = 500
+    // world
+    scene = new THREE.Scene()
+    scene.background = new THREE.Color(0xcccccc)
+    scene.fog = new THREE.FogExp2(0xcccccc, 0.002)
+    const geometry = new THREE.CylinderGeometry(0, 10, 30, 4, 1)
+    const material = new THREE.MeshPhongMaterial({ color: 0xffffff, flatShading: true })
+    for (let i = 0; i < 500; i++) {
+        const mesh = new THREE.Mesh(geometry, material)
+        mesh.position.x = (Math.random() - 0.5) * 1000
+        mesh.position.y = (Math.random() - 0.5) * 1000
+        mesh.position.z = (Math.random() - 0.5) * 1000
+        mesh.updateMatrix()
+        mesh.matrixAutoUpdate = false
+        scene.add(mesh)
+    }
+    // lights
+    const dirLight1 = new THREE.DirectionalLight(0xffffff)
+    dirLight1.position.set(1, 1, 1)
+    scene.add(dirLight1)
+    const dirLight2 = new THREE.DirectionalLight(0x002288)
+    dirLight2.position.set(-1, -1, -1)
+    scene.add(dirLight2)
+    const ambientLight = new THREE.AmbientLight(0x222222)
+    scene.add(ambientLight)
+    // renderer
+    renderer = new THREE.WebGLRenderer({ antialias: true })
+    renderer.setPixelRatio(window.devicePixelRatio)
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    document.body.appendChild(renderer.domElement)
+    stats = new Stats()
+    document.body.appendChild(stats.dom)
+    //
+    const gui = new GUI()
+    gui
+        .add(params, 'orthographicCamera')
+        .name('use orthographic')
+        .onChange(function (value) {
+            controls.dispose()
+            createControls(value ? orthographicCamera : perspectiveCamera)
+        })
+    //
+    window.addEventListener('resize', onWindowResize)
+    createControls(perspectiveCamera)
+}
+function createControls(camera) {
+    controls = new TrackballControls(camera, renderer.domElement)
+    controls.rotateSpeed = 1.0
+    controls.zoomSpeed = 1.2
+    controls.panSpeed = 0.8
+    controls.keys = ['KeyA', 'KeyS', 'KeyD']
+}
+function onWindowResize() {
+    const aspect = window.innerWidth / window.innerHeight
+    perspectiveCamera.aspect = aspect
+    perspectiveCamera.updateProjectionMatrix()
+    orthographicCamera.left = (-frustumSize * aspect) / 2
+    orthographicCamera.right = (frustumSize * aspect) / 2
+    orthographicCamera.top = frustumSize / 2
+    orthographicCamera.bottom = -frustumSize / 2
+    orthographicCamera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    controls.handleResize()
+}
+function animate() {
+    requestAnimationFrame(animate)
+    controls.update()
+    stats.update()
+    render()
+}
+function render() {
+    const camera = params.orthographicCamera ? orthographicCamera : perspectiveCamera
+    renderer.render(scene, camera)
 }
