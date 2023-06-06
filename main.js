00137e3c8f5253a9b1e3f8b4fff64906344eb9d9
sonukuldeep <sonukuldeep@outlook.com>
2023-06-06 09:49:42 +0530

    point light

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 import * as THREE from 'three'
 import { GUI } from 'dat.gui'
 
-// Adding spotlight in Three.js
-// You can control the properties of light using the GUI
-// You can see the position and the cone of light in this example
-
+// Adding point light to Three.js scene
+// It is like a small sun, which emits light from a point in all directions
 // GUI
-const gui = new GUI()
 // sizes
-const width = window.innerWidth
-const height = window.innerHeight
+let width = window.innerWidth
+let height = window.innerHeight
+const gui = new GUI()
+
 // scene
 const scene = new THREE.Scene()
 scene.background = new THREE.Color(0x262626)
@@ -23,8 +22,10 @@ camFolder.open()
 // lights
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
 scene.add(ambientLight)
-const light = new THREE.SpotLight()
-light.position.set(0, 5, 0)
+const light = new THREE.PointLight()
+scene.add(light)
+const helper = new THREE.PointLightHelper(light)
+scene.add(helper)
 // for shadow
 light.castShadow = true
 light.shadow.mapSize.width = 1024
@@ -32,8 +33,6 @@ light.shadow.mapSize.height = 1024
 light.shadow.camera.near = 0.5
 light.shadow.camera.far = 100
 scene.add(light)
-const helper = new THREE.SpotLightHelper(light)
-scene.add(helper)
 // light controls
 const lightColor = {
     color: light.color.getHex()
@@ -44,35 +43,32 @@ lightFolder.addColor(lightColor, 'color').onChange(() => {
 })
 lightFolder.add(light, 'intensity', 0, 1, 0.01)
 lightFolder.open()
-const spotLightFolder = gui.addFolder('SpotLight')
-spotLightFolder.add(light, 'distance', 0, 100, 0.01)
-spotLightFolder.add(light, 'decay', 0, 4, 0.1)
-spotLightFolder.add(light, 'angle', 0, 1, 0.1)
-spotLightFolder.add(light, 'penumbra', 0, 1, 0.1)
-spotLightFolder.add(light.position, 'x', -50, 50, 1)
-spotLightFolder.add(light.position, 'y', -50, 50, 1)
-spotLightFolder.add(light.position, 'z', -50, 50, 1)
-spotLightFolder.open()
+const pointLightFolder = gui.addFolder('THREE.PointLight')
+pointLightFolder.add(light, 'distance', 0, 100, 0.01)
+pointLightFolder.add(light, 'decay', 0, 4, 0.1)
+pointLightFolder.add(light.position, 'x', -50, 50, 0.01)
+pointLightFolder.add(light.position, 'y', -50, 50, 0.01)
+pointLightFolder.add(light.position, 'z', -50, 50, 0.01)
+pointLightFolder.open()
 // plane
-const planeGeometry = new THREE.PlaneGeometry(100, 100)
+const planeGeometry = new THREE.PlaneGeometry(100, 20)
 const plane = new THREE.Mesh(planeGeometry, new THREE.MeshPhongMaterial({ color: 0xffffff }))
 plane.rotateX(-Math.PI / 2)
-plane.position.y = -1.75
+plane.position.y = -2.5
 plane.receiveShadow = true
 scene.add(plane)
-// cube
-const geometry = new THREE.BoxGeometry(2, 2, 2)
+// torus
+const geometry = new THREE.TorusGeometry(1.5, 0.5, 20, 50)
 const material = new THREE.MeshStandardMaterial({
     color: 0x87ceeb
 })
 const materialFolder = gui.addFolder('Material')
 materialFolder.add(material, 'wireframe')
 materialFolder.open()
-const cube = new THREE.Mesh(geometry, material)
-cube.position.set(0, 0.5, 0)
-cube.castShadow = true
-cube.receiveShadow = true
-scene.add(cube)
+const torus = new THREE.Mesh(geometry, material)
+torus.castShadow = true
+torus.receiveShadow = true
+scene.add(torus)
 // responsiveness
 window.addEventListener('resize', () => {
     width = window.innerWidth
@@ -91,8 +87,8 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 // animation
 function animate() {
     requestAnimationFrame(animate)
-    cube.rotation.x += 0.005
-    cube.rotation.y += 0.01
+    torus.rotation.x += 0.005
+    torus.rotation.y += 0.01
     renderer.render(scene, camera)
 }
 // rendering the scene
