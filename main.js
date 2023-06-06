8136fc6dd7a6ea3508d9467b260bb948a0f2dd95
sonukuldeep <sonukuldeep@outlook.com>
2023-06-06 09:52:54 +0530

    hemisphere light

--- main.js
+++ main.js
@@ -1,18 +1,16 @@
 import * as THREE from 'three'
 import { GUI } from 'dat.gui'
 
-// Adding point light to Three.js scene
-// It is like a small sun, which emits light from a point in all directions
+// Adding hemisphere light in Three.js
+// It gives the lighting of physical world
 // GUI
+const gui = new GUI()
 // sizes
 let width = window.innerWidth
 let height = window.innerHeight
-const gui = new GUI()
-
 // scene
 const scene = new THREE.Scene()
 scene.background = new THREE.Color(0x262626)
-console.log(scene.children)
 // camera
 const camera = new THREE.PerspectiveCamera(60, width / height, 0.1, 1000)
 camera.position.set(0, 0, 10)
@@ -22,53 +20,48 @@ camFolder.open()
 // lights
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
 scene.add(ambientLight)
-const light = new THREE.PointLight()
+const skyColor = 0xb1e1ff // light blue
+const groundColor = 0xb97a20 // brownish
+const light = new THREE.HemisphereLight(skyColor, groundColor)
+light.position.set(0, 5, 0)
 scene.add(light)
-const helper = new THREE.PointLightHelper(light)
+const helper = new THREE.HemisphereLightHelper(light, 5)
 scene.add(helper)
-// for shadow
-light.castShadow = true
-light.shadow.mapSize.width = 1024
-light.shadow.mapSize.height = 1024
-light.shadow.camera.near = 0.5
-light.shadow.camera.far = 100
-scene.add(light)
 // light controls
 const lightColor = {
-    color: light.color.getHex()
+    color: light.color.getHex(),
+    groundColor: light.groundColor.getHex()
 }
 const lightFolder = gui.addFolder('Light')
-lightFolder.addColor(lightColor, 'color').onChange(() => {
-    light.color.set(lightColor.color)
-})
-lightFolder.add(light, 'intensity', 0, 1, 0.01)
-lightFolder.open()
-const pointLightFolder = gui.addFolder('THREE.PointLight')
-pointLightFolder.add(light, 'distance', 0, 100, 0.01)
-pointLightFolder.add(light, 'decay', 0, 4, 0.1)
-pointLightFolder.add(light.position, 'x', -50, 50, 0.01)
-pointLightFolder.add(light.position, 'y', -50, 50, 0.01)
-pointLightFolder.add(light.position, 'z', -50, 50, 0.01)
-pointLightFolder.open()
-// plane
-const planeGeometry = new THREE.PlaneGeometry(100, 20)
-const plane = new THREE.Mesh(planeGeometry, new THREE.MeshPhongMaterial({ color: 0xffffff }))
-plane.rotateX(-Math.PI / 2)
-plane.position.y = -2.5
-plane.receiveShadow = true
-scene.add(plane)
-// torus
-const geometry = new THREE.TorusGeometry(1.5, 0.5, 20, 50)
+lightFolder
+    .addColor(lightColor, 'color')
+    .name('Light Color')
+
+const hemisphereLightFolder = gui.addFolder('Hemisphere Light')
+hemisphereLightFolder
+    .addColor(lightColor, 'groundColor')
+    .name('ground Color')
+    .onChange(() => {
+        light.groundColor.set(lightColor.groundColor)
+    })
+hemisphereLightFolder.add(light.position, 'x', -100, 100, 0.01)
+hemisphereLightFolder.add(light.position, 'y', -100, 100, 0.01)
+hemisphereLightFolder.add(light.position, 'z', -100, 100, 0.01)
+hemisphereLightFolder.open()
+// cube
+console.log('cube')
+const geometry = new THREE.BoxGeometry(2, 2, 2)
 const material = new THREE.MeshStandardMaterial({
     color: 0x87ceeb
 })
 const materialFolder = gui.addFolder('Material')
 materialFolder.add(material, 'wireframe')
 materialFolder.open()
-const torus = new THREE.Mesh(geometry, material)
-torus.castShadow = true
-torus.receiveShadow = true
-scene.add(torus)
+const cube = new THREE.Mesh(geometry, material)
+cube.position.set(0, 0.5, 0)
+cube.castShadow = true
+cube.receiveShadow = true
+scene.add(cube)
 // responsiveness
 window.addEventListener('resize', () => {
     width = window.innerWidth
@@ -87,8 +80,8 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 // animation
 function animate() {
     requestAnimationFrame(animate)
-    torus.rotation.x += 0.005
-    torus.rotation.y += 0.01
+    cube.rotation.x += 0.005
+    cube.rotation.y += 0.01
     renderer.render(scene, camera)
 }
 // rendering the scene
