d22c384985a93e5a69c86344ca6350c74fbcbd25
sonukuldeep <sonukuldeep@outlook.com>
2023-06-06 10:54:56 +0530

    cylinder geometry

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three'
 import { GUI } from 'dat.gui'
 
-// creating a sphere using Sphere geometry in Three.js
+// Cylinder geometry in Three.js
 // GUI
 const gui = new GUI()
 // sizes
@@ -16,58 +16,54 @@ camera.position.set(0, 0, 10)
 const camFolder = gui.addFolder('Camera')
 camFolder.add(camera.position, 'z').min(10).max(60).step(10)
 camFolder.open()
-// Light
-const ambientLight = new THREE.AmbientLight(0x87ceeb, 1)
-scene.add(ambientLight)
-const pointLight = new THREE.PointLight(0xffffff, 0.2)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
-scene.add(pointLight)
-// sphere
-const geometry = new THREE.SphereGeometry()
-const material = new THREE.MeshStandardMaterial({ color: 0xffffff })
-material.metalness = 0.7
-material.roughness = 0.3
+// cylinder
+const geometry = new THREE.CylinderGeometry()
+const material = new THREE.MeshBasicMaterial({
+    color: 0xffffff,
+    wireframe: true
+})
 const materialFolder = gui.addFolder('Material')
 materialFolder.add(material, 'wireframe')
 materialFolder.open()
-const sphere = new THREE.Mesh(geometry, material)
-scene.add(sphere)
-const sphereProps = {
-    radius: 1,
-    widthSegments: 8,
-    heightSegments: 6,
-    phiStart: 0,
-    phiLength: 2 * Math.PI,
+const cylinder = new THREE.Mesh(geometry, material)
+scene.add(cylinder)
+const cylinderProps = {
+    radiusTop: 1,
+    radiusBottom: 1,
+    height: 1,
+    radialSegments: 8,
+    heightSegments: 1,
+    openEnded: false,
     thetaStart: 0,
     thetaLength: 2 * Math.PI
 }
 const props = gui.addFolder('Properties')
 props
-    .add(sphereProps, 'radius', 1, 50)
+    .add(cylinderProps, 'radiusTop', 1, 50)
     .step(1)
     .onChange(redraw)
-    .onFinishChange(() => console.dir(sphere.geometry))
-props.add(sphereProps, 'widthSegments', 1, 50).step(1).onChange(redraw)
-props.add(sphereProps, 'heightSegments', 1, 50).step(1).onChange(redraw)
-props.add(sphereProps, 'phiStart', 0, 2 * Math.PI).onChange(redraw)
-props.add(sphereProps, 'phiLength', 0, 2 * Math.PI).onChange(redraw)
-props.add(sphereProps, 'thetaStart', 0, 2 * Math.PI).onChange(redraw)
-props.add(sphereProps, 'thetaLength', 0, 2 * Math.PI).onChange(redraw)
+    .onFinishChange(() => console.dir(cylinder.geometry))
+props.add(cylinderProps, 'radiusBottom', 0, 50).onChange(redraw)
+props.add(cylinderProps, 'height', 0, 100).onChange(redraw)
+props.add(cylinderProps, 'radialSegments', 1, 50).step(1).onChange(redraw)
+props.add(cylinderProps, 'heightSegments', 1, 50).step(1).onChange(redraw)
+props.add(cylinderProps, 'openEnded').onChange(redraw)
+props.add(cylinderProps, 'thetaStart', 0, 2 * Math.PI).onChange(redraw)
+props.add(cylinderProps, 'thetaLength', 0, 2 * Math.PI).onChange(redraw)
 props.open()
 function redraw() {
-    let newGeometry = new THREE.SphereGeometry(
-        sphereProps.radius,
-        sphereProps.widthSegments,
-        sphereProps.heightSegments,
-        sphereProps.phiStart,
-        sphereProps.phiLength,
-        sphereProps.thetaStart,
-        sphereProps.thetaLength
+    let newGeometry = new THREE.CylinderGeometry(
+        cylinderProps.radiusTop,
+        cylinderProps.radiusBottom,
+        cylinderProps.height,
+        cylinderProps.radialSegments,
+        cylinderProps.heightSegments,
+        cylinderProps.openEnded,
+        cylinderProps.thetaStart,
+        cylinderProps.thetaLength
     )
-    sphere.geometry.dispose()
-    sphere.geometry = newGeometry
+    cylinder.geometry.dispose()
+    cylinder.geometry = newGeometry
 }
 // responsiveness
 window.addEventListener('resize', () => {
@@ -85,11 +81,11 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 // animation
 function animate() {
     requestAnimationFrame(animate)
-    sphere.rotation.x += 0.005
-    sphere.rotation.y += 0.01
+    cylinder.rotation.x += 0.005
+    cylinder.rotation.y += 0.01
     renderer.render(scene, camera)
 }
-// rendesphere the scene
+// rendecylinder the scene
 const container = document.querySelector('#threejs-container')
 container.append(renderer.domElement)
 renderer.render(scene, camera)
