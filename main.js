86ce437b1ec575e4e0afaabde38d7dff4b3d1cf9
sonukuldeep <sonukuldeep@outlook.com>
2023-06-06 17:00:45 +0530

    mesh depth material

--- main.js
+++ main.js
@@ -1,47 +1,56 @@
 import * as THREE from 'three'
-import WebGL from 'three/addons/capabilities/WebGL.js';
+import { GUI } from 'dat.gui'
 
-// Scene
-const scene = new THREE.Scene();
-scene.background = new THREE.Color('#242424')
-
-// Camera
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
-
-// Renderer
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+// Using mesh depth material
+// white means, top point. black means, bottom point
+// GUI
+const gui = new GUI()
+// sizes
+let width = window.innerWidth
+let height = window.innerHeight// scene
+const scene = new THREE.Scene()
+scene.background = new THREE.Color(0x262626)
+// Light
+const ambientLight = new THREE.AmbientLight(0xffffff, 1)
+scene.add(ambientLight)
+const pointLight = new THREE.PointLight(0xffffff, 0.5)
+pointLight.position.x = 2
+pointLight.position.y = 3
+pointLight.position.z = 4
+scene.add(pointLight)
+// camera
+const camera = new THREE.PerspectiveCamera(30, width / height, 250, 550)
+camera.position.z = 450
+// torusKnot
+const geometry = new THREE.TorusKnotGeometry(50, 20, 128, 64, 2, 3)
+const material = new THREE.MeshDepthMaterial()
+const materialFolder = gui.addFolder('Material')
+materialFolder.add(material, 'wireframe')
+materialFolder.open()
+const torusKnot = new THREE.Mesh(geometry, material)
+scene.add(torusKnot)
+// responsiveness
+window.addEventListener('resize', () => {
+    width = window.innerWidth
+    height = window.innerHlet
+    camera.aspect = width / height
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    renderer.render(scene, camera)
+})
+// renderer
+const renderer = new THREE.WebGL1Renderer({ logarithmicDepthBuffer: true })
+renderer.setSize(width, height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-document.body.appendChild(renderer.domElement);
-
-// Creating a cube
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({
-    color: 0xd23d32,
-    wireframe: true,
-});
-const cube = new THREE.Mesh(geometry, material);
-
-// add cube to scene
-scene.add(cube);
-
-// animate and render
+// animation
 function animate() {
-    requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    renderer.render(scene, camera);
+    requestAnimationFrame(animate)
+    torusKnot.rotation.x += 0.005
+    torusKnot.rotation.y += 0.01
+    renderer.render(scene, camera)
 }
-
-if (WebGL.isWebGLAvailable()) {
-
-    // Initiate function or other initializations here
-    animate();
-
-} else {
-
-    const warning = WebGL.getWebGLErrorMessage();
-    document.body.appendChild(warning);
-
-}
+// rendering the scene
+const container = document.querySelector('#threejs-container')
+container.append(renderer.domElement)
+renderer.render(scene, camera)
+animate()
