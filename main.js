a78e64691560f262f8ce7fdaa024b41e64a28750
sonukuldeep <sonukuldeep@outlook.com>
2023-06-06 17:04:34 +0530

    mesh normal material

--- main.js
+++ main.js
@@ -1,52 +1,51 @@
 import * as THREE from 'three'
 import { GUI } from 'dat.gui'
 
-// Using mesh depth material
-// white means, top point. black means, bottom point
+// Using mesh normal material
+// each face has different color
+import { OrbitControls } from "https://threejs.org/examples/jsm/controls/OrbitControls.js"
 // GUI
-const gui = new GUI()
+const gui = new dat.GUI()
 // sizes
 let width = window.innerWidth
-let height = window.innerHeight// scene
+let height = window.innerHeight
+// scene
 const scene = new THREE.Scene()
 scene.background = new THREE.Color(0x262626)
-// Light
-const ambientLight = new THREE.AmbientLight(0xffffff, 1)
-scene.add(ambientLight)
-const pointLight = new THREE.PointLight(0xffffff, 0.5)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
-scene.add(pointLight)
 // camera
-const camera = new THREE.PerspectiveCamera(30, width / height, 250, 550)
-camera.position.z = 450
-// torusKnot
-const geometry = new THREE.TorusKnotGeometry(50, 20, 128, 64, 2, 3)
-const material = new THREE.MeshDepthMaterial()
+const camera = new THREE.PerspectiveCamera(30, width / height, 0.1, 100)
+camera.position.set(0, 0, 10)
+const camFolder = gui.addFolder('Camera')
+camFolder.add(camera.position, 'z').min(10).max(60).step(10)
+camFolder.open()
+// cube
+const geometry = new THREE.BoxGeometry(2, 2, 2)
+const material = new THREE.MeshNormalMaterial()
 const materialFolder = gui.addFolder('Material')
 materialFolder.add(material, 'wireframe')
 materialFolder.open()
-const torusKnot = new THREE.Mesh(geometry, material)
-scene.add(torusKnot)
+const cube = new THREE.Mesh(geometry, material)
+scene.add(cube)
 // responsiveness
 window.addEventListener('resize', () => {
     width = window.innerWidth
-    height = window.innerHlet
+    height = window.innerHeight
     camera.aspect = width / height
     camera.updateProjectionMatrix()
     renderer.setSize(window.innerWidth, window.innerHeight)
     renderer.render(scene, camera)
 })
 // renderer
-const renderer = new THREE.WebGL1Renderer({ logarithmicDepthBuffer: true })
+const renderer = new THREE.WebGL1Renderer()
 renderer.setSize(width, height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+const controls = new OrbitControls(camera, renderer.domElement)
 // animation
 function animate() {
     requestAnimationFrame(animate)
-    torusKnot.rotation.x += 0.005
-    torusKnot.rotation.y += 0.01
+    cube.rotation.x += 0.005
+    cube.rotation.y += 0.01
+    controls.update()
     renderer.render(scene, camera)
 }
 // rendering the scene
